e57cbd5963eb3e607192180672ed462fd80e9ddb
fahimahammed <fahimahammed.cse@gmail.com>
2023-08-30 11:36:45 +0600

    fix

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -25,7 +25,7 @@ export const hasTimeConflict = (
         const existingStart = new Date(`1970-01-01T${slot.startTime}:00`);
         const existingEnd = new Date(`1970-01-01T${slot.endTime}:00`);
         const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
-        const newEnd = new Date(`1970-01-01T${newSlot.startTime}:00`);
+        const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
 
         if (newStart < existingEnd && newEnd > existingStart) {
             return true
