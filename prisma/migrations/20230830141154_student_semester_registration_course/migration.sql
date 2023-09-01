98dd12e1de2e189aa5aa1f9adc16223b52437603
fahimahammed <fahimahammed.cse@gmail.com>
2023-09-01 22:55:34 +0600

    enroll, withdraw and confirm my course

---
+++ prisma/migrations/20230830141154_student_semester_registration_course/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "student_semester_registration_courses" (
+    "semesterRegistrationId" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "offeredCourseId" TEXT NOT NULL,
+    "offeredCourseSectionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "student_semester_registration_courses_pkey" PRIMARY KEY ("semesterRegistrationId","studentId","offeredCourseId")
+);
--- src/app/modules/semesterRegistration/semesterRegistration.interface.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.interface.ts
@@ -2,3 +2,8 @@ export type ISemesterRegistrationFilterRequest = {
     searchTerm?: string | undefined;
     academicSemesterId?: string | undefined;
 }
+
+export type IEnrollCoursePayload = {
+    offeredCourseId: string,
+    offeredCourseSectionId: string
+}
