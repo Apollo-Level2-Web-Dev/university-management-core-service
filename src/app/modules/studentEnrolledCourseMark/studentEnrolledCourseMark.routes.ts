8178f1be3ed47bdd3ab20b8c2c9f70046fb8e69f
fahimahammed <fahimahammed.cse@gmail.com>
2023-09-11 17:24:52 +0600

    added

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { StudentEnrolledCourseMarkConroller } from './studentEnrolledCourseMark.controller';
+import { StudentEnrolledCourseMarkValidation } from './studentEnrolledCourseMark.validations';
 
 const router = express.Router();
 
@@ -11,7 +13,23 @@ router.get(
     StudentEnrolledCourseMarkConroller.getAllFromDB
 );
 
-router.patch('/update-marks', StudentEnrolledCourseMarkConroller.updateStudentMarks)
-router.patch('/update-final-marks', StudentEnrolledCourseMarkConroller.updateFinalMarks)
+router.get(
+    '/my-marks',
+    auth(ENUM_USER_ROLE.STUDENT),
+    StudentEnrolledCourseMarkConroller.getMyCourseMarks
+);
+
+router.patch(
+    '/update-marks',
+    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+    validateRequest(StudentEnrolledCourseMarkValidation.updateStudentMarks),
+    StudentEnrolledCourseMarkConroller.updateStudentMarks
+)
+router.patch(
+    '/update-final-marks',
+    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+    validateRequest(StudentEnrolledCourseMarkValidation.updateStudentMarks),
+    StudentEnrolledCourseMarkConroller.updateFinalMarks
+)
 
 export const studentEnrolledCourseMarkRoutes = router;
